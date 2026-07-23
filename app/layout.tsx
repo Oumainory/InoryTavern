56bbf7386e7b318a79f4880ede15c3232176fd24
Admin <hhffjjgg@163.com>
2026-07-24 06:41:51 +0800

    修复水合错误

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN" suppressHydrationWarning className="h-full antialiased">
-      <body className="min-h-full flex flex-col bg-background text-foreground">
+      <body
+        suppressHydrationWarning
+        className="min-h-full flex flex-col bg-background text-foreground"
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
